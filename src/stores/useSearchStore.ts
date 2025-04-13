46b77251bd7054c86d0d834e3f59b8350b936870
Pablo Pareja <pablo.pareja@strategyzer.com>
2025-04-13 11:14:20 +0200

    add dev tools support for zustand

--- src/stores/useSearchStore.ts
+++ src/stores/useSearchStore.ts
@@ -1,11 +1,17 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 type SearchStore = {
   searchText: string
   setSearchText: (text: string) => void
 }
 
-export const useSearchStore = create<SearchStore>(set => ({
-  searchText: '',
-  setSearchText: text => set({ searchText: text }),
-}))
+export const useSearchStore = create<SearchStore>()(
+  devtools(
+    set => ({
+      searchText: '',
+      setSearchText: text => set({ searchText: text }, false, 'search/setSerchText'), // This is the name of the action that will appear in the dev tools
+    }),
+    { name: 'SearchStore' } // This is the pointer that will appear in dev tools for debugging purposes
+  )
+)
